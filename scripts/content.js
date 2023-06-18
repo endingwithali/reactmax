8c922c03072b918820947954137331cbd60748b8
Ali Diamond <user+github@ali.dev>
2023-06-18 13:21:35 -0400

    webpack is working

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,5 @@
+import Rekognition from "aws-sdk/clients/rekognition"
+
 window.onload = () => {
     let video = document.createElement("video");
     let canvas = document.createElement("canvas");
@@ -27,7 +29,60 @@ window.onload = () => {
     
             document.body.appendChild(video)
             document.body.appendChild(photo)
+
+            processFace(data)
         }
         video.play();
     })
 }
+
+
+
+const aws = new AWS.Rekognition()
+const processFace = async (face) => {
+    //22 char
+    let blob = string.slice(22, len(face))
+    console.log(blob)
+    // let params = {
+    //     "Attributes": [ "EMOTIONS" ],
+    //     "Image": { 
+    //        "Bytes": blob,
+    //     },
+    //  }
+    // aws.detectFaces(params, function(err, response) {
+    //     console.log(response);
+    // })
+}
+
+/* 
+const uniqueName = `${tempFolder}${tempName}.${type.ext}`;
+  const uploadPromise = uploadImage(imageBuffer, uniqueName, type);
+  const tempImageData = await uploadPromise;
+
+  // should this be done uploading to AWS or sent as encoded file
+  console.log("UPLOADED TEMP IMAGE");
+  console.log(tempImageData);
+  const params = {
+    CollectionId: rekognitionCollection,
+    FaceMatchThreshold: recThreshold,
+    Image: {
+      S3Object: {
+        Bucket: imageBucket,
+        Name: uniqueName
+      }
+    }
+  };
+
+  const searchFacesByImagePromise = rekognition
+    .searchFacesByImage(params)
+    .promise();
+
+  const imageData = await searchFacesByImagePromise.catch(error => {
+    console.error("ERROR: problems uploading profile image");
+    console.error(error);
+    throw "Error: recognize_image upload image";
+  });
+
+  return imageData["FaceMatches"][0];
+
+  */
